d54bdd531bca6e125bc90bc54fa26d57ddf5e37a
Tejas <tblamkhade24@gmail.com>
2025-02-05 18:32:04 +0530

    React files committed

---
+++ basic-react/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>My name is Tejas</h1>
+      <br></br>
+      <h1>Hello DevOps World</h1>
+    </div>
+  );
+}
+
+export default App;
